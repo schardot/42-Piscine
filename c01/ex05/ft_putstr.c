158fd6db50af8429188c3bb73f7bac46a70915c7
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-02-27 16:45:25 +0100

    C01 EX05

---
+++ c01/ex05/ft_putstr.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+void ft_putstr(char *str)
+{
+    while (*str != '\0')
+    {
+        write(1, str, 1);
+        str ++;
+    }
+}
+
+// int main(void)
+// {
+//     ft_putstr("mamao");
+// }
