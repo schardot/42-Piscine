f372efe07c7c0bd924c2cc4725099a5f8bb4ec73
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-03-07 11:37:02 +0100

    C03 EX03

--- c03/ex03/ft_strncat.c
+++ c03/ex03/ft_strncat.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
-char *ft_strncat(char *dest, char *src, unsigned int nb)
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (dest[i] != '\0')
-        i++;
-    while (nb > 0 && *src != '\0')
-    {
-        dest[i] = *src;
-        i++;
-        src++;
-        nb--;
-    }
-    dest[i] = '\0';
-    return (dest);
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (nb > 0 && *src != '\0')
+	{
+		dest[i] = *src;
+		i++;
+		src++;
+		nb--;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
-int main(void)
-{
-    char str1[100] = "I love ", str2[] = "youtube";
-    printf("%s\n", ft_strncat(str1, str2, 3));
-    return 0;
-}
-
+// int	main(void)
+// {
+// 	char str1[100] = "I love ", str2[] = "youtube";
+// 	printf("%s\n", ft_strncat(str1, str2, 3));
+// 	return 0;
+// }
