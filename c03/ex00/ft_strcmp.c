81ec2175445d84704195a291e9a79a417e6b5511
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-03-07 10:02:15 +0100

    norminetted

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -1,11 +1,11 @@
-int ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(char *s1, char *s2)
 {
-    while (*s1 != '\0' && *s2 != '\0')
-    {
-        if (*s1 != *s2)
-            break;
-        s1++;
-        s2++;
-    }
-    return (*s1 - *s2);
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			break ;
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
