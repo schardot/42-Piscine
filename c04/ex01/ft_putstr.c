fd48964f3ba004ed795b94c0ffa3aad233b34704
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-04-02 14:37:37 +0200

    C04 EX01 ft_putstr.c

---
+++ c04/ex01/ft_putstr.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_putchar(*str);
+		str ++;
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+// int main(void)
+// {
+//     ft_putstr("hello");
+// }
