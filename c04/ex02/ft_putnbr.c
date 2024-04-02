9d9314e1e8ef92fc3b8e6689e7d83541b3be4129
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-04-02 15:02:50 +0200

    C04 EX02 ft_putnbr.c

--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -1,25 +1,25 @@
 #include <unistd.h>
 
-void ft_putchar(char c);
+void	ft_putchar(char c);
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-    long longnb;
-    longnb = nb;
-    if (nb < 0)
-    {
-        ft_putchar('-');
-        longnb = longnb * -1;
-    }
-    if (longnb >= 10)
-    {
-        ft_putnbr(longnb / 10);
-    }
+	long	longnb;
 
-    ft_putchar(longnb % 10 + '0');
+	longnb = nb;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		longnb = longnb * -1;
+	}
+	if (longnb >= 10)
+	{
+		ft_putnbr(longnb / 10);
+	}
+	ft_putchar(longnb % 10 + '0');
 }
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
