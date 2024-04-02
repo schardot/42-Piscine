fadba44fbc31f71af9aefebc79d62161b24bbd62
Natália Schardosim <natalia.schardosim@hotmail.com>
2024-04-02 16:07:37 +0200

    C04 EX04 ft_putnbr_base.c / falta lidar com -2147483648 em binário

---
+++ c04/ex04/ft_putnbr_base.c
@@ -0,0 +1,102 @@
+#include <unistd.h>
+#include <limits.h>
+
+int ft_checkbase(char *base);
+int isnegative(int nbr);
+int ft_strcmp(char *s1, char *s2);
+long ft_expo(long n, long powerof);
+
+void ft_putnbr_base(int nbr, char *base)
+{
+    long b;
+    long div;
+    int round;
+    long number;
+
+    round = 0;
+    b = ft_checkbase(base);
+    if (b <= 1)
+        return;
+    number = (long)isnegative(nbr);
+    if (number == 0)
+    {
+        write(1, &base[0], 1);
+        return;
+    }
+    while (b > 0 && number >= ft_expo(b, round))
+        round++;
+    while (round >= 0)
+    {
+        div = number / (ft_expo(b, round));
+        number %= (ft_expo(b, round));
+        write(1, &base[div], 1);
+        round --;
+    }
+}
+
+int ft_checkbase(char *b)
+{
+    int i;
+
+    i = -1;
+    while (i++, b[i] != '\0')
+    {
+        if (b[i] == '+' || b[i] == '-' || b[i] == b[i - 1])
+            return (1);
+    }
+    if (i < 2)
+        return (1);
+    return (i);
+}
+
+int isnegative(int nbr)
+{
+    long number;
+
+    number = nbr;
+    if (number == INT_MIN)
+    {
+        write(1, "-", 1);
+        number = -(long)INT_MIN;
+    }
+    else if (number < 0)
+    {
+        write(1, "-", 1);
+        number = -number;
+    }
+    return (number);
+}
+
+int ft_strcmp(char *s1, char *s2)
+{
+    int i;
+
+    i = 0;
+    while (s1[i] != '\0' || s2[i] != '\0')
+    {
+        if (s1[i] != s2[i])
+        {
+            return (s1[i] - s2[i]);
+        }
+        i++;
+    }
+    return (0);
+}
+
+long ft_expo(long n, long powerof)
+{
+    long number;
+
+    number = 1;
+    while (powerof > 0)
+    {
+        number *= n;
+        powerof--;
+    }
+    return (number);
+}
+
+int main(void)
+{
+    ft_putnbr_base(214748364, "01");
+}
